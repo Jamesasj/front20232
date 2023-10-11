785fdbac2f46e5db0d9fdca5388ec4ef7309e042
James Alves <james@jamesalves.com.br>
2023-10-10 21:44:40 -0300

    bootstrap jquery

--- aula7/js/main.js
+++ aula7/js/main.js
@@ -3,20 +3,12 @@ console.log('inicio do app');
 fetch('https://jsonplaceholder.typicode.com/users').then(res => {
     return res.json();
 }).then(function (body) {
-    const table = document.querySelector(".table");
     body.forEach(element => {
-        const div = document.createElement('div');
-        div.innerHTML = `${element.id} - ${element.name} - ${element.email}`
-        table.appendChild(div)
-    });
+        $('.tableContent').append(
+            $('<tr></tr>')
+                .append($('<th></th>', { scope: "row", text: element.id }))
+                .append($('<td></td>', { text: element.name }))
+                .append($('<td></td>', { text: element.email }))
+        );
+    })
 })
-
-console.log('aqui esta fora da promisses')
-
-
-console.log('inicio  promisses await')
-const execawiat = await fetch('https://jsonplaceholder.typicode.com/users');
-console.log('meio  promisses await')
-const novoObj = await execawiat.json();
-console.log('fim  promisses await')
-console.table(novoObj)
