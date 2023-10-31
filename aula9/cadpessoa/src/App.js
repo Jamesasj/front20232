a69e8b479c0a783ceb0f729224b7f1ede18d5974
James Alves <james@jamesalves.com.br>
2023-10-31 19:57:27 -0300

    add local storage

--- aula9/cadpessoa/src/App.js
+++ aula9/cadpessoa/src/App.js
@@ -1,18 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 import './style/main.css'
+import { useState } from 'react';
+
+
+
 function App() {
+
+  const [pessoa, setPessoa] = useState({ nome: 'Alex', cpf: 123 });
+  const [msg, setMsg] = useState('')
+  const minhaFuncao = x => {
+    setMsg('usuario incluido com sucesso!')
+    let pessoas = JSON.parse(localStorage.getItem('pessoas')) || [];
+    pessoas.push(pessoa);
+    localStorage.setItem('pessoas', JSON.stringify(pessoas))
+  }
+
   return (
     <div className='container'>
+      <div className='toast'>
+        <div>{msg}</div>
+      </div>
       <div className='combo'>
         <label>Nome</label>
-        <input type='text' placeholder='nome'></input>
+        <input type='text' id='nome'
+          placeholder='nome'
+          value={pessoa.nome}
+          onChange={({ target }) => {
+            console.log(target.value)
+            setPessoa({
+              ...pessoa,
+              nome: target.value
+            });
+          }}></input>
       </div>
       <div className='combo'>
         <label>CPF</label>
-        <input  type='text' placeholder='CPF'></input>
+        <input type='number' placeholder='CPF'
+          value={pessoa.cpf}
+          onChange={({ target }) => {
+            setPessoa({ ...pessoa, cpf: target.value })
+          }}></input>
       </div>
-      <button>Salvar</button>
+      <div className='combo'>
+        <h1>{pessoa.nome}</h1>
+      </div>
+      <button onClick={minhaFuncao}>Salvar</button>
     </div>
   );
 }
